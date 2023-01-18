fb37c29e379ce3eec1d41b227f26c57205a3b880
Greene, Calyn <CalynGreene@rocketmortgage.com>
2023-01-17 21:44:17 -0500

    created shop page with mock data

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -7,9 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ShopComponent {
   
-  public nftImg1 = ".../../../assets/nfts/Image 1-17-23 at 6.25 PM.jpg";
-  public nftImg2 = "../../../assets/nfts/Image 1-17-23 at 6.26 PM.jpg";
-
   public nfts:Array<any> = [
     {img: "../../../assets/nfts/Image 1-17-23 at 6.26 PM.jpg", name: "Name", description: "Description", price: "Price"},
     {img: ".../../../assets/nfts/Image 1-17-23 at 6.25 PM.jpg", name: "Name", description: "Description", price: "Price"},
@@ -35,6 +32,5 @@ export class ShopComponent {
     {img: ".../../../assets/nfts/Image 1-17-23 at 6.25 PM.jpg", name: "Name", description: "Description", price: "Price"},
     {img: "../../../assets/nfts/Image 1-17-23 at 6.26 PM.jpg", name: "Name", description: "Description", price: "Price"},
     {img: ".../../../assets/nfts/Image 1-17-23 at 6.25 PM.jpg", name: "Name", description: "Description", price: "Price"},
-   
   ]
 }
