9fd71f7b27a272c6da9936ceeb681e503cc7b6d3
vphillips1 <verkeiaphillips@rocketmortgage.com>
2023-01-20 22:43:31 -0500

    added button routes and updated css/html for shop

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  constructor(public router: Router) {}
   public navbarLinks = [
     {
       name: 'Shop',
-      route: '/login/shop'
+      route: '/login/shop',
     },
     {
       name: 'Checkout',
-      route: '/login/checkout'
-    }
+      route: '/login/checkout',
+    },
   ];
-
 }
