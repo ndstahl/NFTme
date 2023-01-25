498828df72177d0d5946d563b23248aa23175186
Admin <ajencoremusic@gmail.com>
2023-01-25 00:09:40 -0500

    multiple changes

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -4,22 +4,88 @@ import { NftMeService } from 'src/app/services/nft-me.service';
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent {
-
   public nfts: any[] = [];
-  
+
+  public purchaseItems: any[] = [];
+
   constructor(private nftMeService: NftMeService) {}
 
   ngOnInit(): void {
-
-    this.nftMeService.getNftData().subscribe((res:any) => {
-      console.log('DATA' , res.data);
+    this.nftMeService.getNftData().subscribe((res: any) => {
+      console.log('DATA', res.data);
       this.nfts = res.data;
       this.nftMeService.$nftMeData.next({
         nfts: res.data,
       });
     });
+    // this.nfts = this.nftsMock;
+    //this.nftMeService.$nftMeData.next({
+    // nfts: this.nftsMock,
+    //});
+  }
+  purchaseNFT(item: any) {
+    console.log('item', item);
+    this.purchaseItems.push(item);
+    this.nftMeService.$nftMeData.next(this.purchaseItems);
   }
 }
+
+/*public nftsMock: any[] = [
+  {
+    id: 1,
+    nftimg:
+      'https://mpb-site.s3.us-east-2.amazonaws.com/imgs/winter-22/n2.jpg',
+    title: 'ReflectLake Helloe',
+    info: 'Where do you go when you to reflect?',
+    category: 'nature',
+    price: '1bitcoin',
+  },
+  {
+    id: 2,
+    nftimg:
+      'https://mpb-site.s3.us-east-2.amazonaws.com/imgs/winter-22/n2.jpg',
+    title: 'DayBreak',
+    info: 'nftInfo',
+    category: 'Animal',
+    price: '1bitcoin',
+  },
+  {
+    id: 3,
+    nftimg:
+      'https://mpb-site.s3.us-east-2.amazonaws.com/imgs/winter-22/n2.jpg',
+    title: 'Ear',
+    info: 'nftInfo',
+    category: 'Animal',
+    price: '1bitcoin',
+  },
+  {
+    id: 4,
+    nftimg:
+      'https://mpb-site.s3.us-east-2.amazonaws.com/imgs/winter-22/n2.jpg',
+    title: 'DayBreak',
+    info: 'nftInfo',
+    category: 'Animal',
+    price: '1bitcoin',
+  },
+  {
+    id: 5,
+    nftimg:
+      'https://mpb-site.s3.us-east-2/amazonaws.com/imgs/winter-22/n2.jpg',
+    title: 'DayBreak',
+    info: 'nftInfo',
+    category: 'Animal',
+    price: '1bitcoin',
+  },
+  {
+    id: 6,
+    nftimg:
+      'https://mpb-site.s3.us-east-2/amazonaws.com/imgs/winter-22/n2.jpg',
+    title: 'DayBreak',
+    info: 'nftInfo',
+    category: 'Animal',
+    price: '1bitcoin',
+  },
+]; */
